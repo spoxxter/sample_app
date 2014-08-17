c4eea466c609bd4df78e35e0ece67f6e225290bb
Jason Sisi <jason.sisi@gmail.com>
2014-08-17 11:29:21 -0700

    Add a full_title helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,8 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'rspec/autorun'
+  #no longer required
+  #require 'rspec/autorun'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
